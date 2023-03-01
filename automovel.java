db49e99cf95ad3b35fecb985dc338c4fde4fe513
Juliane de Fatima Neves dos Santos <88359786+JulianeNeves@users.noreply.github.com>
2023-03-01 09:10:00 -0300

    Create automovel.java

---
+++ automovel.java
@@ -0,0 +1,17 @@
+package src;
+
+public class automovel {
+	
+	String marca;
+	String modelo;
+	String cor;
+	double preco;
+	
+	public void Informa_automovel() {
+		System.out.println("marca: " + marca);
+		System.out.println("modelo: " + modelo);
+		System.out.println("cor: " + cor);
+		System.out.println("preco: " + preco);
+	}
+
+}
